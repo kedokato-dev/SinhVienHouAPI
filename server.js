77c846a9cd39a0432db06d23c777a597efe9e024
Trần Anh Quân <thocodeanhquan@gmail.com>
2025-04-15 13:51:41 +0700

    📡ping server every 5 minutes 📡

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const app = require('./src/index');
+const axios = require('axios');
 
 const PORT = process.env.PORT || 3000;
 
+
+const pingServer = () => {
+    const serverUrl = `https://sinhvienhouapi.onrender.com`;
+    setInterval(async () => {
+      try {
+        const response = await axios.get(serverUrl);
+        console.log(`✅ Ping thành công đến ${serverUrl}: ${response.status}`);
+      } catch (error) {
+        console.error(`❌ Lỗi khi ping: ${error.message}`);
+      }
+    }, 5 * 60 * 1000); // Ping mỗi 5 phút
+  };
+
 app.listen(PORT, () => {
   console.log(`✅ Server đang chạy tại http://localhost:${PORT}`);
+  pingServer(); 
 });
